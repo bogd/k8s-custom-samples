3354198e850be64c14777d006b1a6a648d7da59f
Bogdan Sass <bogd.no.spam@gmail.com>
2024-03-27 11:00:03 +0200

    Operators

--- 08-operators/setup_elasticsearch_eck.sh
+++ 08-operators/setup_elasticsearch_eck.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-oc create -f https://download.elastic.co/downloads/eck/2.11.0/crds.yaml
+LATEST_VERSION=$(curl -s https://api.github.com/repos/elastic/cloud-on-k8s/releases | jq '.[].tag_name' -r | head -n 1) 
+
+oc create -f https://download.elastic.co/downloads/eck/${LATEST_VERSION#v}/crds.yaml
 echo "Waiting for CRDs..."
 until kubectl get elasticsearch.elasticsearch ; do date; sleep 1; echo ""; done
-oc create -f https://download.elastic.co/downloads/eck/2.11.0/operator.yaml
+oc create -f https://download.elastic.co/downloads/eck/${LATEST_VERSION#v}/operator.yaml
